eadb292c71eec32004b75cebb194d9171036ac8f
jaykher13 <kherjay220@gmail.com>
2024-07-31 06:58:10 +0000

    detatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = "14433"
+let accountEmail = "jaykher22@gmail.com"
+var accountPasword = "12345"
+accountCity = "amreli"
+let accountState = "rajkot"
+
+// accountId = 2
+
+accountEmail = "abc@gmail.com"
+accountPasword = "67890"
+accountCity = "surat"
+
+/*
+don't use var
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPasword, accountCity, accountState]);
